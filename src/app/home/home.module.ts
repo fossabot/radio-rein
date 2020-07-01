d962026482a0193bc0ef84ec01e01e119a63e43b
Domenik Töfflinger <domenikt96@yahoo.de>
2020-07-01 20:16:09 +0200

    ui updates

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -11,6 +11,7 @@ import { ApiService } from '../core/api.service';
 import { SwiperModule } from 'ngx-swiper-wrapper';
 import { RecordingsModule } from '@app/recordings/recordings.module';
 import { ShowsModule } from '@app/shows/shows.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
@@ -22,6 +23,7 @@ import { ShowsModule } from '@app/shows/shows.module';
     SwiperModule,
     RecordingsModule,
     ShowsModule,
+    NgbModule,
   ],
   declarations: [HomeComponent],
   providers: [ApiService, Title],
